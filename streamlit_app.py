fe902711f6aa914555d06217248465fa701a7bef
Felipe Ormazabal <97199418+felipeorma@users.noreply.github.com>
2025-03-22 10:16:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ if uploaded_file:
     df = pd.read_excel(uploaded_file)
 
     keywords_by_role = {
-        'Goalkeepe': ['GK'],
+        'Goalkeeper': ['GK'],
         'Defender': ['CB', 'RCB', 'LCB'],
         'Fullback': ['LB', 'RB', 'LWB', 'RWB'],
         'Midfielder': ['CMF', 'DMF', 'AMF', 'LMF', 'RMF'],
